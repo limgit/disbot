86604a0559bc346c52094afb86df856bfd0eb879
Hyunsu Lim <hyunsu.lim01@gmail.com>
2020-10-25 15:10:46 +0900

    Love only simple ones

--- src/core/commands/money.ts
+++ src/core/commands/money.ts
@@ -21,10 +21,10 @@ const money: CustomCommand = {
   description: '돈 정산을 해줘요!',
   aliases: ['m'],
   usage: [
-    { description: '(별칭: ls) 최근 n개의 트랜잭션을 보여줍니다. 이름(들)이 주어질 경우 해당 인물(들)과 관계된 트랜잭션만 보여집니다.', args: 'list <n> [이름] [이름]' },
-    { description: '(별칭: st) 현재 채무 상태를 보여줍니다. 이름이 주어질 경우 해당 인물의 채무 상태를 보여줍니다.', args: 'status [이름]' },
-    { description: '(별칭: t) 트랜잭션을 추가합니다', args: 'transaction <금액(원)> <준 사람> <받은 사람> [사유]' },
-    { description: '(별칭: d) 더치페이 정보를 추가합니다', args: 'dutch <총 금액(원)> <돈 낸 사람> <돈 낸 사람 제외 더치페이 참여자 목록(쉼표 구분)> [사유]' },
+    { description: '최근 n개의 트랜잭션을 보여줍니다. 이름(들)이 주어질 경우 해당 인물(들)과 관계된 트랜잭션만 보여집니다.', args: 'ls <n> [이름] [이름]' },
+    { description: '현재 채무 상태를 보여줍니다. 이름이 주어질 경우 해당 인물의 채무 상태를 보여줍니다.', args: 'st [이름]' },
+    { description: '트랜잭션을 추가합니다', args: 't <금액(원)> <준 사람> <받은 사람> [사유]' },
+    { description: '더치페이 정보를 추가합니다', args: 'd <총 금액(원)> <돈 낸 사람> <돈 낸 사람 제외 더치페이 참여자 목록(쉼표 구분)> [사유]' },
     { description: '채무 청산에 최소 트랜잭션을 발생시키는 방법을 출력합니다', args: 'plan <청산할 사람 목록(쉼표 구분)>' },
     { description: '채무를 청산합니다', args: 'clear <청산할 사람 목록(쉼표 구분)>' },
   ],
@@ -32,7 +32,7 @@ const money: CustomCommand = {
     if (argv.length === 1) {
       return message.reply('money 명령어는 최소 하나의 인자가 필요합니다. `!help money`를 통해 자세한 사용법을 확인할 수 있습니다.');
     }
-    if (argv[1] === 'list' || argv[1] === 'ls') {
+    if (argv[1] === 'ls') {
       if (isNaN(argv[2] as any)) return message.reply('트랜잭션 갯수는 반드시 숫자여야 합니다');
       const limit = Number(argv[2]);
       if (argv[3] && !validateName(message, argv[3])) return;
@@ -55,7 +55,7 @@ const money: CustomCommand = {
           .setDescription(logs.join('\n'));
         message.channel.send(embed);
       });
-    } else if (argv[1] === 'status' || argv[1] === 'st') {
+    } else if (argv[1] === 'st') {
       if (argv[2] && !validateName(message, argv[2])) return;
       const promise = argv[2] ? db.getBalances(argv[2]) : db.getBalances();
       promise.then((rows) => {
@@ -87,14 +87,14 @@ const money: CustomCommand = {
         });
         message.channel.send(embed);
       });
-    } else if (argv[1] === 'transaction' || argv[1] === 't') {
+    } else if (argv[1] === 't') {
       if (isNaN(argv[2] as any)) return message.reply('금액은 반드시 숫자여야 합니다');
       if (!validateName(message, argv[3]) || !validateName(message, argv[4])) return;
       const reason = argv[5] ?? '';
       db.addTransaction(argv[3], argv[4], reason, Number(argv[2])).then(() => {
         message.reply('트랜잭션이 추가되었습니다');
       });
-    } else if (argv[1] === 'dutch' || argv[1] === 'd') {
+    } else if (argv[1] === 'd') {
       if (isNaN(argv[2] as any)) return message.reply('금액은 반드시 숫자여야 합니다');
       if (!validateName(message, argv[3])) return;
       const people = argv[4].split(',');
